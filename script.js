b688bcf7d3b9a817a83cc758acd448c717afff04
Hpandya837 <callnirav1234@gmail.com>
2023-12-19 14:58:26 -0500

    added delay

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function shakeMagic8Ball() {
     const question = prompt('Ask the Magic 8-Ball a question:');
+
     if (question === null) {
         document.getElementById('response-text').innerText = 'Please ask a question!'
         document.getElementById('response-text').classList = 'text-danger'
-        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' //FIX THIS
+        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' // FIX THIS
         return
     }
 
@@ -12,62 +13,65 @@ function shakeMagic8Ball() {
         return
     }
 
-    let start = '';
-    if (!start) {
-        document.getElementById('shakingBacon').classList.add('shaking');
-    }
+    const responseImage = document.getElementById('response-image');
+    responseImage.classList.add('shaking');
 
-    const randomNumber = Math.floor(Math.random() * 8);
-    let answer, image, color;
+    // Add a delay before displaying the answer
+    setTimeout(() => {
+        const randomNumber = Math.floor(Math.random() * 8);
+        let answer, image, color;
 
-    switch (randomNumber) {
-        case 0:
-            answer = "Yes";
-            image = "yes-image.jpg" //change this!!;
-            color = "text-success";
-            break;
-        case 1:
-            answer = "No";
-            image = "no-image.jpg" //change this!!;
-            color = "text-danger";
-            break;
-        case 2:
-            answer = "Ask again later";
-            image = "later-image.jpg" //change this!!;
-            color = "text-secondary";
-            break;
-        case 3:
-            answer = "Maybe";
-            image = "maybe-image.jpg";//change this!!;
-            color = "text-warning";
-            break;
-        case 4:
-            answer = "Cannot predict now";
-            image = "cannot-predict-image.jpg";//change this!!;
-            color = "text-muted";
-            break;
-        case 5:
-            answer = "Don't count on it";
-            image = "dont-count-on-it-image.jpg";//change this!!;
-            color = "text-danger";
-            break;
-        case 6:
-            answer = "Most likely";
-            image = "most-likely-image.jpg";//change this!!;
-            color = "text-success";
-            break;
-        case 7:
-            answer = "Outlook not so good";
-            image = "outlook-not-good-image.jpg";//change this!!;
-            color = "text-danger";
-            break;
-        default:
-            break;
-    }
+        switch (randomNumber) {
+            case 0:
+                answer = "Yes";
+                image = "yes-image.jpg";
+                color = "text-success";
+                break;
+            case 1:
+                answer = "No";
+                image = "no-image.jpg";
+                color = "text-danger";
+                break;
+            case 2:
+                answer = "Ask again later";
+                image = "later-image.jpg";
+                color = "text-secondary";
+                break;
+            case 3:
+                answer = "Maybe";
+                image = "maybe-image.jpg";
+                color = "text-warning";
+                break;
+            case 4:
+                answer = "Cannot predict now";
+                image = "cannot-predict-image.jpg";
+                color = "text-muted";
+                break;
+            case 5:
+                answer = "Don't count on it";
+                image = "dont-count-on-it-image.jpg";
+                color = "text-danger";
+                break;
+            case 6:
+                answer = "Most likely";
+                image = "most-likely-image.jpg";
+                color = "text-success";
+                break;
+            case 7:
+                answer = "Outlook not so good";
+                image = "outlook-not-good-image.jpg";
+                color = "text-danger";
+                break;
+            default:
+                break;
+        }
 
-const responseText = document.getElementById('response-text');
-responseText.innerText = answer;
-responseText.classList = `display-4 ${color}`;
-document.getElementById('response-image').src = image;
+        const responseText = document.getElementById('response-text');
+        responseText.innerText = answer;
+        responseText.classList = `display-4 ${color}`;
+        responseImage.src = image;
 
+        // Remove the shaking class after displaying the answer
+        responseImage.classList.remove('shaking');
+    }, 3000); // Adjust the delay time (in milliseconds) as needed
 }
